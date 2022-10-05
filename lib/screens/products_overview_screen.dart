8ac317332aa448210b78fef9bdb0de95108ae837
Adrián Fernández Pérez <adrifernandezperez12@gmail.com>
2022-10-05 17:30:00 +0200

    Añadiendo carrito

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../widgets/products_grid.dart';
+import 'package:shop_app/widgets/badge.dart';
+
+import '../providers/cart.dart';
 import '../providers/products.dart';
+import '../widgets/products_grid.dart';
 
 enum FilterOptions {
   Favorites,
@@ -14,7 +17,8 @@ class ProductsOverviewScreen extends StatefulWidget {
 }
 
 class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
-  var _showOnlyFavorites= false;
+  var _showOnlyFavorites = false;
+
   @override
   Widget build(BuildContext context) {
     final productsContainer = Provider.of<Products>(context, listen: false);
@@ -25,17 +29,30 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
           PopupMenuButton(
             onSelected: (FilterOptions selectedValue) {
               setState(() {
-                if (selectedValue == FilterOptions.Favorites){
+                if (selectedValue == FilterOptions.Favorites) {
                   _showOnlyFavorites = true;
                 } else {
                   _showOnlyFavorites = false;
                 }
               });
             },
-            icon: Icon(Icons.more_vert,), itemBuilder: (_) => [
-            PopupMenuItem(child: Text('Only Favorites'), value: FilterOptions.Favorites),
-            PopupMenuItem(child: Text('Show All'), value: FilterOptions.All),
-          ],)
+            icon: Icon(
+              Icons.more_vert,
+            ),
+            itemBuilder: (_) => [
+              PopupMenuItem(child: Text('Only Favorites'), value: FilterOptions.Favorites),
+              PopupMenuItem(child: Text('Show All'), value: FilterOptions.All),
+            ],
+          ),
+          Consumer<Cart>(
+              builder: (_, cartData, ch) => Badge(
+                child: ch as Widget,
+                value: cartData.itemCount.toString(),
+              ),
+              child: IconButton(
+                icon: Icon(Icons.shopping_cart),
+                onPressed: () {},
+              ))
         ],
       ),
       body: ProductsGrid(_showOnlyFavorites),
